21c31442aa6f44ef5648b3da60938e97227a951c
Sujith Quintelier <sujith.quintelier@gmail.com>
2022-02-01 14:58:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='toolbox',
     version='0.0.3',
-    author='Mike Huls',
-    author_email='mike_huls@hotmail.com',
+    author='Sujith Quintelier',
+    author_email='sujith.quintelier#gmail.com',
     description='Testing installation of Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Muls/toolbox',
+    url='https://github.com/sujithq/toolbox',
     project_urls = {
-        "Bug Tracker": "https://github.com/Muls/toolbox/issues"
+        "Bug Tracker": "https://github.com/sujithq/toolbox/issues"
     },
     license='MIT',
     packages=['toolbox'],
